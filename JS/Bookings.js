fca4b7c1da83f8014a2222666a3e6c78f5987af7
Rick <RickHarrisIT@gmx.com>
2017-11-13 21:14:20 +0000

    booking form:alert box

--- JS/Bookings.js
+++ JS/Bookings.js
@@ -1,17 +1,19 @@
 
 
-    $(function() {
-        $("#datepicker").datepicker({ dateFormat: "dd-mm-yy" }).val()
-});
+//     $(function() {
+//         $("#datepicker").datepicker({ dateFormat: "dd-mm-yy" }).val()
+// });
 
 
-function FormValidation (fname,lname,location,date,tel,emails){
-    var fname= document.forms["Booking"]["fname"].value;
-    var lname= document.forms["Booking"]["lname"].value;
-    var location= document.forms["Booking"]["location"].value;
-    var date= document.forms["Booking"]["date"].value;
-    var tel= document.forms["Booking"]["tel"].value;
-    var email= document.forms["Booking"]["email"].value;
+function formValidation(){
+    alert("Booking request submitted,we will reply soon");
+    var fname = document.forms["Booking"]["fname"].value;
+    var lname = document.forms["Booking"]["lname"].value;
+    var location = document.forms["Booking"]["location"].value;
+    var date = document.forms["Booking"]["date"].value;
+    var tel = document.forms["Booking"]["tel"].value;
+    var email = document.forms["Booking"]["email"].value;
+    
     if (fname==""){
         alert("first name required");
         return false;}
@@ -31,5 +33,5 @@ function FormValidation (fname,lname,location,date,tel,emails){
         alert("email required");
         return false;}
         
-    alert("Booking request submitted,we will reply soon")
+    alert("Booking request submitted,we will reply soon");
 }
